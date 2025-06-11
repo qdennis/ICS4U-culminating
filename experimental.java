b7c21bc135b69dbeb68d1f384a8cf06524ad5d7b
Pigduck12 <eliot013@hotmail.com>
2025-06-10 22:56:58 -0400

    Add files via upload
    uhh base classes

---
+++ experimental.java
@@ -0,0 +1,33 @@
+public class experimental{
+    public class transactions{
+
+    }
+    public class user{
+        String userId;
+
+    String name;
+
+    String email;
+
+    String password;
+
+    String cardNumber;
+
+    }
+    public void Login(){
+
+    }
+    public void Logout(){
+
+    }
+    public class Customer extends user{
+
+    }
+    public class bank{
+
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
